000fcf49e0e07f86b60f717044cede5cba85ba92
Paul Pelletier <paul.pelletier@esterfinance.com>
2021-03-03 16:56:42 +0100

    Ajout d'une seconde ligne WriteLine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ABC");
+            Console.WriteLine("Modification 1");
+            Console.WriteLine("Modification 2");
+
         }
     }
 }
